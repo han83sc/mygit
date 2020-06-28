51a9ba8c879eb146287c6be675fede88930e4c8a
Ann Liao <han.83sc@icloud.com>
2020-06-28 17:26:41 +0800

    commit 2

--- Case1_JSON/py_getJSON.py
+++ Case1_JSON/py_getJSON.py
@@ -3,7 +3,7 @@
 
 import requests
 import json
-data = requests.get(url="http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3")
+data = requests.get(url="https://quality.data.gov.tw/dq_download_json.php?nid=127631&md5_url=5582f416b5f008d6faf08ed4c9dfd7aa")
 with open("music.json","w",encoding="utf-8") as myFile:
     json.dump(data.json(), myFile,ensure_ascii=False)
 myFile.close()
